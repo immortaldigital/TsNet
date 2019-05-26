6560c8499cffb6e214ec978a7be45301cde28350
immortaldigital <38929472+immortaldigital@users.noreply.github.com>
2019-05-27 05:59:29 +1000

    Pre-compiled JS files

---
+++ Compiled/Neuron.js
@@ -0,0 +1,79 @@
+/*
+* Neuron.ts
+* 2019 Immortal.Digital
+* Provides a list of Synapses + bias
+* fire() takes the sum X of all supplied input[i] values multiplied by all Synapse[i] values and returns sigmoid(X)
+* train() calculates the derivative of each weight[i] with respect to the ideal output and updates the weight value
+* train() has 2 modes, output layer and hidden layer:
+*     Output mode accepts raw target output value, calculates Dx with respect to current node only and returns the sum of WeightDxOutput
+*     Hidden mode accepts derived output value, calculates Dx with respect to all output nodes and returns the sum of WeightDxOutput
+*/
+import { Synapse } from './Synapse.js';
+export class Neuron {
+    get Output() {
+        return this.output;
+    }
+    constructor(size) {
+        //initialise all arrays and variables
+        this.weight = [];
+        this.totalErrorDxNeuronOutput = 0.0;
+        this.neuronDxNeuronInput = 0.0;
+        this.neuronInputDxWeight = [];
+        this.totalErrorDxWeight = [];
+        this.backPropogationDxWeight = [];
+        //initialise synapses
+        for (let i = 0; i < size; i++) {
+            this.weight[i] = new Synapse();
+        }
+        this.bias = new Synapse();
+    }
+    fire(data) {
+        this.output = 0.0;
+        this.input = data; //reference to previous Layer[i-1].Output[]
+        //output is Sigmoid((input[i] * weight[i]).Sum + bias)
+        if (this.input.length == this.weight.length) {
+            for (var i = 0; i < this.input.length; i++) {
+                this.output += (this.input[i] * this.weight[i].WUpdate); //use WUpdate which means it will use the updated value
+            }
+            this.output += (this.bias.W * 1.0);
+        }
+        this.output = Neuron.sigmoid(this.output); //all hidden layer + output layer have sigmoid function. Input layer doesnt use neurons
+        return this.output;
+    }
+    train(target, outputLayer, learningRate) {
+        /* Calculate following for this neuron where k is each weight attached to this neuron:
+         *	totalErrorDxNeuronOutput	= total error change Dx with respect to Neuron[this] output
+         *	neuronDxNeuronInput			= Neuron[this] output change Dx with respect to Neuron[this] net input
+         *	neuronInputDxWeight[k]		= Neuron[this] net input change Dx with respect to weight[k]
+         *	totalErrorDxWeight[k]		= total error change partial derivative with respect to neuron.weight[k]
+         *
+         * Equations:
+         *	totalErrorDxNeuronOutput:	TotalError = (0.5 * (target - actual) ^ 2) =>>= (Dx = -(target - actual))
+         *	neuronDxNeuronInput:		NeuronOutput = Sigmoid(NeuronInput) =>>= SigmoidDx(NeuronInput)
+         *	neuronInputDxWeight[k]:		NeuronInput[o] = PrevOut[o] * Weight[k] =>>= PrevOut[l] where o is neuron o attached to weight [k]
+         *	totalErrorDxWeight[k]:		totalErrorDxNeuronOutput * neuronDxNeuronInput * neuronInputDxWeight[k]
+        */
+        if (outputLayer) {
+            this.totalErrorDxNeuronOutput = -(target - this.output);
+        }
+        else {
+            this.totalErrorDxNeuronOutput = target; //the partial derivative is pre-calculated for hidden layers
+        }
+        this.neuronDxNeuronInput = Neuron.sigmoidDx(this.output);
+        for (var k = 0; k < this.input.length; k++) {
+            this.neuronInputDxWeight[k] = this.input[k];
+            this.totalErrorDxWeight[k] = this.totalErrorDxNeuronOutput * this.neuronDxNeuronInput * this.neuronInputDxWeight[k];
+            //.W is old weight, .W will only be updated to WUpdate when TsNet.Fire() is called. Continue to access the old weight while training the network
+            this.weight[k].WUpdate = (this.weight[k].W - (this.totalErrorDxWeight[k] * learningRate));
+            //back propogate the value totalErrorDxNeuronOutput[o] where o is the neuron attached to weight[k]
+            this.backPropogationDxWeight[k] = (this.totalErrorDxNeuronOutput * this.neuronDxNeuronInput * this.weight[k].W);
+        }
+        return this.backPropogationDxWeight; //backwards error propogation values with respect to weights
+    }
+    static sigmoid(x) {
+        return 1.0 / (1.0 + Math.exp(-x)); //exp(x) = e^x
+    }
+    static sigmoidDx(x) {
+        return x * (1 - x); //derivative of sigmoid
+    }
+}
